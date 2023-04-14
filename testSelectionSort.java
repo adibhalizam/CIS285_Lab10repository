cdbd93344c6d7467a251e042d7cd02e9d7ebef3c
Muhammad Adib Bin Halizam <adibhali@umich.edu>
2023-04-13 22:49:23 -0400

    Software Engineer 2 work

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -44,11 +44,39 @@ public class testSelectionSort {
 	}
 	
 	public void testMixed(){
-		/** Test data contains with both positive, negative and zeros **/
+		int[] arr = new int[5];
+		arr[0] = 0;
+		arr[1] = -9;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = -2;
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -9;
+		Sortedarr[1] = -2;
+		Sortedarr[2] = 0;
+		Sortedarr[3] = 7;
+		Sortedarr[4] = 10;
+		SelectionSort temp = new SelectionSort();
+		int sortedActual[] = temp.basicSelectionSort(arr);
+		assertArrayEquals(Sortedarr, sortedActual);
 	}
 	
 	public void testDuplicates(){
-		/** Test data contains duplicates **/
+		int[] arr = new int[5];
+		arr[0] = 0;
+		arr[1] = -9;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 7;
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -9;
+		Sortedarr[1] = 0;
+		Sortedarr[2] = 7;
+		Sortedarr[3] = 7;
+		Sortedarr[4] = 10;
+		SelectionSort temp = new SelectionSort();
+		int sortedActual[] = temp.basicSelectionSort(arr);
+		assertArrayEquals(Sortedarr, sortedActual);
 	}
 	
 }
