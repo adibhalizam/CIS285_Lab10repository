98670b4b102a53b452199b8d1a092ba37818f77d
Muhammad Adib Bin Halizam <adibhali@umich.edu>
2023-04-13 22:41:25 -0400

    SoftwareEngineer1 work

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -36,11 +36,28 @@ public class testSelectionSort {
 		Sortedarr[2] = 8;
 		Sortedarr[3] = 9;
 		Sortedarr[4] = 10;
-		/** add tests to check for this unit test **/
+		SelectionSort temp = new SelectionSort();
+		int sortedActual[] = temp.basicSelectionSort(arr);
+		assertArrayEquals(Sortedarr, sortedActual);
 	}
 	
 	public void testNegative(){
-		/** Test data contains negative values only **/
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = -9;
+		arr[2] = -7;
+		arr[3] = -10;
+		arr[4] = -2;
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -10;
+		Sortedarr[1] = -9;
+		Sortedarr[2] = -8;
+		Sortedarr[3] = -7;
+		Sortedarr[4] = -2;
+		SelectionSort temp = new SelectionSort();
+		int sortedActual[] = temp.basicSelectionSort(arr);
+		assertArrayEquals(Sortedarr, sortedActual);
+
 	}
 	
 	public void testMixed(){
